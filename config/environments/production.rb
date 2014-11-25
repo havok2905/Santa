7101718b93eee151140b00c689b95a70dd1d6a00
Christopher McLean <havok2905@gmail.com>
2014-11-25 14:59:50 -0500

    more mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,23 +73,6 @@ Santa::Application.configure do
   config.active_support.deprecation = :notify
 
 
-
-
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'https://protected-ravine-2227.herokuapp.com',
-    user_name:            'havok2905@gmail.com',
-    password:             'ee5ahcqh',
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
-
-
-
-
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
